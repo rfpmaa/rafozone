adbdaa6ee08dcad9817ea7674cd49ad80e942df9
Oryza Sekar <sekaroryza@gmail.com>
2026-04-26 15:59:40 +0700

    oza

--- app/Views/customer/menu_makanan.php
+++ app/Views/customer/menu_makanan.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-md-4 mb-4">
                 <div class="card card-layanan h-100 border-0">
-                    <img src="url_gambar_kamu.jpg" class="card-img-top menu-img" alt="Menu" style="border-radius: 20px 20px 0 0;">
+                    <img src="public\images\indomie.jpg" class="card-img-top menu-img" alt="Menu" style="border-radius: 20px 20px 0 0;">
                     <div class="card-body">
                         <h5 class="font-weight-bold text-white mb-2">Indomie Goreng + Telur</h5>
                         <h6 class="text-primary font-weight-bold mb-3">Rp 10.000</h6>
